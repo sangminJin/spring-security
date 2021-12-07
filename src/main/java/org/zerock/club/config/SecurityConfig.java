14042637cdc433df9c7e700984f218bc34b4187a
jinsangmin <sangminjin2@gmail.com>
2021-12-07 16:23:29 +0900

    회원 엔티티 생성 및 테스트

--- src/main/java/org/zerock/club/config/SecurityConfig.java
+++ src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package org.zerock.club.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,4 +18,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+        
+        http.formLogin();   // 인가,인증에 문제시 로그인 화면
+        http.csrf().disable();
+        http.logout();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication().withUser("user1")
+                .password("$2a$10$uhWIybupCrQiv4aVKCUPKeWTojm/6zVt2If9ZQ1h/M8E9RDCZ7.E2")
+                .roles("USER");
+
+    }
 }
